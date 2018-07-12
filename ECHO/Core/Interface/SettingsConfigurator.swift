40800a18436dce6e95e19bc80b61d4a4054a440f
Fedorenko Nikita <n.fedorenko@pixelplex.by>
2018-07-12 15:06:09 +0300

    api options set added

--- ECHO/Core/Interface/SettingsConfigurator.swift
+++ ECHO/Core/Interface/SettingsConfigurator.swift
@@ -6,10 +6,19 @@
 //  Copyright © 2018 PixelPlex. All rights reserved.
 //
 
+struct APIOption : OptionSet {
+    let rawValue: Int
+    
+    static let firstOption  = APIOption(rawValue: 1 << 0)
+    static let secondOption = APIOption(rawValue: 1 << 1)
+    static let thirdOption  = APIOption(rawValue: 1 << 2)
+}
+
 class Configurator {
-    var url: String?
-    var socketMessenger: SocketMessenger?
-    var cryproComponent: CryptoCoreComponent?
+    var url: String = ""
+    var socketMessenger: SocketMessenger = SocketMessengerImp()
+    var cryproComponent: CryptoCoreComponent = CryptoCoreComponentImp()
+    var apiOptions: APIOption = [.firstOption,.secondOption,.thirdOption]
 }
 
 class Settings {
@@ -17,15 +26,17 @@ class Settings {
     let url: String
     let socketMessenger: SocketMessenger
     let cryproComponent: CryptoCoreComponent
-    
+    let apiOptions: APIOption?
+
     typealias BuildConfiguratorClosure = (Configurator) -> Void
     
     init(build: BuildConfiguratorClosure = {_ in }) {
         
         let configurator = Configurator()
         build(configurator)
-        url = configurator.url ?? ""
-        socketMessenger = configurator.socketMessenger ?? SocketMessengerImp()
-        cryproComponent = configurator.cryproComponent ?? CryptoCoreComponentImp()
+        url = configurator.url
+        socketMessenger = configurator.socketMessenger
+        cryproComponent = configurator.cryproComponent
+        apiOptions = configurator.apiOptions
     }
 }
