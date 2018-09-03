67c27be6bf63d4da640c14a2e02e42622ee80ed7
Fedorenko Nikita <n.fedorenko@pixelplex.io>
2018-09-03 19:37:38 +0300

    message encryption and decryption tests added

--- ECHOTests/CryptoCoreComponentTests.swift
+++ ECHOTests/CryptoCoreComponentTests.swift
@@ -48,4 +48,50 @@ class CryptoCoreComponentTests: XCTestCase {
         //assert
         XCTAssertEqual(hashed.hex, "4d06f9499e922b268bf868db6ea7f185f142a7eeb89aa2db98c074ea16dc2b4a")
     }
+    
+    func testEncryptMessage() {
+        
+        //arrange
+        let privateKey = Data(hex: "f33b293c58b55e7a34878d5e2fcc5b82624bef424a0ce282be10069a4a60eae5")!
+        let publicKey = Data(hex: "028d4927dde3607d75f09532ea825313dba15411f1c6f7ea355ec5265e8ea39fa1")!
+        let message = "Hello World!"
+        let nonce = "0"
+        
+        //act
+        let encryptedMessage = cryptoCore.encryptMessage(privateKey: privateKey, publicKey: publicKey, nonce: nonce, message: message)
+        
+        //assert
+        XCTAssertEqual(encryptedMessage.hex, "eeaaf3119b3342e2c16610ba661ce5cca0ab82c3fd0656b2712ff1016d1f0fdf")
+    }
+    
+    func testDecryptMessage() {
+        
+        //arrange
+        let privateKey = Data(hex: "f33b293c58b55e7a34878d5e2fcc5b82624bef424a0ce282be10069a4a60eae5")!
+        let publicKey = Data(hex: "028d4927dde3607d75f09532ea825313dba15411f1c6f7ea355ec5265e8ea39fa1")!
+        let message = Data(hex: "eeaaf3119b3342e2c16610ba661ce5cca0ab82c3fd0656b2712ff1016d1f0fdf")!
+        let nonce = "0"
+        
+        //act
+        let decryptedMessage = cryptoCore.decryptMessage(privateKey: privateKey, publicKey: publicKey, nonce: nonce, message: message)
+        
+        //assert
+        XCTAssertEqual(decryptedMessage, "Hello World!")
+    }
+    
+    func testEncryptThenDecryptMessage() {
+        
+        //arrange
+        let privateKey = Data(hex: "f33b293c58b55e7a34878d5e2fcc5b82624bef424a0ce282be10069a4a60eae5")!
+        let publicKey = Data(hex: "028d4927dde3607d75f09532ea825313dba15411f1c6f7ea355ec5265e8ea39fa1")!
+        let message = "Hello World!"
+        let nonce = "0"
+        
+        //act
+        let encryptedMessage = cryptoCore.encryptMessage(privateKey: privateKey, publicKey: publicKey, nonce: nonce, message: message)
+        let decryptedMessage = cryptoCore.decryptMessage(privateKey: privateKey, publicKey: publicKey, nonce: nonce, message: encryptedMessage)
+        
+        //assert
+        XCTAssertEqual(message, decryptedMessage)
+    }
 }
