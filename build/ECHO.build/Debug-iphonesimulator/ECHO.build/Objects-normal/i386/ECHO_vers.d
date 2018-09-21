c7c48a7c0dfe7fa02eb5e8211b14aa3ed0f5c68f
Vladimir Sharaev <v.sharaev@pixelplex.io>
2018-09-21 14:14:39 +0300

    Remove build folder from git

--- build/ECHO.build/Debug-iphonesimulator/ECHO.build/Objects-normal/i386/ECHO_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/nikitafedorenko/ECHO-lib/build/ECHO.build/Debug-iphonesimulator/ECHO.build/DerivedSources/ECHO_vers.c
--- build/ECHO.build/Debug-iphonesimulator/ECHO.build/Script-B356019F20F39F4B0097FD38.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if which swiftlint >/dev/null; then
-swiftlint
-else
-echo "error: SwiftLint does not exist, download it from https://github.com/realm/SwiftLint"
-exit 1
-fi
-
