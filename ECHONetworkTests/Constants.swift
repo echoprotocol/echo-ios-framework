574de5c65172bd84b8e832f81371bc67d582eb81
Vladimir Sharaev <v.sharaev@pixelplex.io>
2019-04-19 15:11:40 +0300

    Added change constants by CI params scripts

--- ECHONetworkTests/Constants.swift
+++ ECHONetworkTests/Constants.swift
@@ -48,8 +48,7 @@ struct Constants {
     
     static var nodeUrl: String {
         if let value = Constants.infoForKey(ConstantsKeys.nodeUrlKey) {
-            print("----------> 1")
-            return value
+            return "wss://" + value
         } else {
             return "wss://testnet.echo-dev.io"
         }
@@ -58,7 +57,6 @@ struct Constants {
     static var timeout: Double {
         if let valueString = Constants.infoForKey(ConstantsKeys.timeout),
             let value = Double(valueString) {
-            print("----------> 2")
             return value
         } else {
             return 20
@@ -67,7 +65,6 @@ struct Constants {
     
     static var counterContract: String {
         if let value = Constants.infoForKey(ConstantsKeys.counterContract) {
-            print("----------> 3")
             return value
         } else {
             return "1.16.139"
@@ -76,7 +73,6 @@ struct Constants {
     
     static var logsContract: String {
         if let value = Constants.infoForKey(ConstantsKeys.logsContract) {
-            print("----------> 4")
             return value
         } else {
             return "1.16.141"
@@ -85,7 +81,6 @@ struct Constants {
     
     static var x86Contract: String {
         if let value = Constants.infoForKey(ConstantsKeys.x86Contract) {
-            print("----------> 5")
             return value
         } else {
             return "1.16.0"
@@ -94,7 +89,6 @@ struct Constants {
     
     static var sidechainTransferObject: String {
         if let value = Constants.infoForKey(ConstantsKeys.sidechainTransferObject) {
-            print("----------> 6")
             return value
         } else {
             return "1.19.0"
@@ -103,7 +97,6 @@ struct Constants {
     
     static var defaultName: String {
         if let value = Constants.infoForKey(ConstantsKeys.defaultName) {
-            print("----------> 7")
             return value
         } else {
             return "vsharaev"
@@ -112,7 +105,6 @@ struct Constants {
     
     static var defaultToName: String {
         if let value = Constants.infoForKey(ConstantsKeys.defaultToName) {
-            print("----------> 8")
             return value
         } else {
             return "vsharaev1"
@@ -121,7 +113,6 @@ struct Constants {
     
     static var defaultPass: String {
         if let value = Constants.infoForKey(ConstantsKeys.defaultPass) {
-            print("----------> 9")
             return value
         } else {
             return "vsharaev"
@@ -130,7 +121,6 @@ struct Constants {
     
     static var defaultAsset: String {
         if let value = Constants.infoForKey(ConstantsKeys.defaultAsset) {
-            print("----------> 10")
             return value
         } else {
             return "1.3.0"
@@ -139,7 +129,6 @@ struct Constants {
     
     static var defaultAssetLowerBound: String {
         if let value = Constants.infoForKey(ConstantsKeys.defaultAssetLowerBound) {
-            print("----------> 11")
             return value
         } else {
             return "ECHO"
@@ -148,7 +137,6 @@ struct Constants {
     
     static var defaultAnotherAsset: String {
         if let value = Constants.infoForKey(ConstantsKeys.defaultAnotherAsset) {
-            print("----------> 12")
             return value
         } else {
             return "1.3.20"
@@ -157,7 +145,6 @@ struct Constants {
     
     static var defaultETHAddress: String {
         if let value = Constants.infoForKey(ConstantsKeys.defaultETHAddress) {
-            print("----------> 13")
             return value
         } else {
             return "0x46Ba2677a1c982B329A81f60Cf90fBA2E8CA9fA8"
@@ -166,7 +153,6 @@ struct Constants {
     
     static var defaultCallContractMethod: String {
         if let value = Constants.infoForKey(ConstantsKeys.defaultCallContractMethod) {
-            print("----------> 14")
             return value
         } else {
             return "incrementCounter"
@@ -175,7 +161,6 @@ struct Constants {
     
     static var defaultCallContractBytecode: String {
         if let value = Constants.infoForKey(ConstantsKeys.defaultCallContractBytecode) {
-            print("----------> 15")
             return value
         } else {
             return "5b34b966"
@@ -184,7 +169,6 @@ struct Constants {
     
     static var defaultQueryContractMethod: String {
         if let value = Constants.infoForKey(ConstantsKeys.defaultQueryContractMethod) {
-            print("----------> 16")
             return value
         } else {
             return "getCount"
@@ -193,7 +177,6 @@ struct Constants {
     
     static var defaultQueryContractBytecode: String {
         if let value = Constants.infoForKey(ConstantsKeys.defaultQueryContractBytecode) {
-            print("----------> 17")
             return value
         } else {
             return "a87d942c"
@@ -202,7 +185,6 @@ struct Constants {
     
     static var evmContractResult: String {
         if let value = Constants.infoForKey(ConstantsKeys.evmContractResult) {
-            print("----------> 18")
             return value
         } else {
             return "1.17.362"
@@ -211,7 +193,6 @@ struct Constants {
     
     static var x86ContractResult: String {
         if let value = Constants.infoForKey(ConstantsKeys.x86ContractResult) {
-            print("----------> 19")
             return value
         } else {
             return "1.17.0"
@@ -221,7 +202,6 @@ struct Constants {
     static var contractLogsFromBlock: Int {
         if let valueString = Constants.infoForKey(ConstantsKeys.contractLogsFromBlock),
             let value = Int(valueString) {
-            print("----------> 20")
             return value
         } else {
             return 62102
@@ -231,7 +211,6 @@ struct Constants {
     static var contractLogsToBlock: Int {
         if let valueString = Constants.infoForKey(ConstantsKeys.contractLogsToBlock),
             let value = Int(valueString) {
-            print("----------> 21")
             return value
         } else {
             return 62103
@@ -240,7 +219,6 @@ struct Constants {
     
     static var logContractByteCode: String {
         if let value = Constants.infoForKey(ConstantsKeys.logContractByteCode) {
-            print("----------> 22")
             return value
         } else {
             return "6080604052348015600f57600080fd5b5061010b8061001f6000396000f300608060405260043610603f576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806329e99f07146044575b600080fd5b348015604f57600080fd5b50606c60048036038101908080359060200190929190505050606e565b005b7fa7659801d76e732d0b4c81221c99e5cf387816232f81f4ff646ba0653d65507a436040518082815260200191505060405180910390a17fa7659801d76e732d0b4c81221c99e5cf387816232f81f4ff646ba0653d65507a816040518082815260200191505060405180910390a1505600a165627a7a723058202ee0ce6afeec3577644075456c235804fa783dcdbb61a981edaf50333f5ef6710029"
@@ -249,7 +227,6 @@ struct Constants {
     
     static var counterContractByteCode: String {
         if let value = Constants.infoForKey(ConstantsKeys.counterContractByteCode) {
-            print("----------> 23")
             return value
         } else {
             return "60806040526000805534801561001457600080fd5b50610101806100246000396000f3006080604052600436106053576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680635b34b966146058578063a87d942c14606c578063f5c5ad83146094575b600080fd5b348015606357600080fd5b50606a60a8565b005b348015607757600080fd5b50607e60ba565b6040518082815260200191505060405180910390f35b348015609f57600080fd5b5060a660c3565b005b60016000808282540192505081905550565b60008054905090565b600160008082825403925050819055505600a165627a7a7230582063e27ea8b308defeeb50719f281e50a9b53ffa155e56f3249856ef7eafeb09e90029"
